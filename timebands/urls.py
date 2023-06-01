9573ac7b728640b28ef703c7cd93c927a869af83
Hanseok Jeong <75151336+hsjeong625@users.noreply.github.com>
2023-06-01 15:43:10 +0900

    Delete timebands directory

--- timebands/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('run/', views.run_task, name='runTask'),
-    # path('call/', views.call_task, name='callTask')
-]
